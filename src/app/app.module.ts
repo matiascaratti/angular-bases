5219c4ca273327577a9a3b63a4779ee11441055e
AMA0456 <matias.caratti@externos-ar.cencosud.com>
2024-06-25 12:20:00 -0300

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
